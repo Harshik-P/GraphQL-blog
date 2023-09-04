ff0d1dd0077260f443a66ad78f832a60a54858ed
Harshik-P <harshik.pulavarthy@gmail.com>
2023-09-04 12:50:43 +0530

    issue in dummy data resolved

--- db/init.sql
+++ db/init.sql
@@ -5,29 +5,29 @@ CREATE DATABASE blogapp;
 \c blogapp;
 
 -- Create the "User" table
-CREATE TABLE IF NOT EXISTS "User" (
+CREATE TABLE IF NOT EXISTS "Users" (
   id VARCHAR(255) NOT NULL,
   username VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP DEFAULT NOW(),
-  updatedAt TIMESTAMP DEFAULT NOW(),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY (id)
 );
 
 -- Create the "Post" table
-CREATE TABLE IF NOT EXISTS "Post" (
+CREATE TABLE IF NOT EXISTS "Posts" (
   id TEXT NOT NULL PRIMARY KEY,
   title TEXT NOT NULL,
   content TEXT NOT NULL,
   author_id VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP DEFAULT NOW(),
-  updatedAt TIMESTAMP DEFAULT NOW()
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
 -- Insert dummy user data
-INSERT INTO "User" (id, username, name, email, password, createdAt, updatedAt)
+INSERT INTO "Users" (id, username, name, email, password, "createdAt", "updatedAt")
 VALUES
     ('1', 'User1', 'John Doe', 'user1@example.com', 'password1', NOW(), NOW()),
     ('2', 'User2', 'Jane Smith', 'user2@example.com', 'password2', NOW(), NOW()),
@@ -38,7 +38,7 @@ VALUES
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Insert dummy post data
-INSERT INTO "Post" (id, title, content, author_id, createdAt, updatedAt)
+INSERT INTO "Posts" (id, title, content, author_id, "createdAt", "updatedAt")
 SELECT
   uuid_generate_v4(),
   'Dummy Title' || generate_series(1, 100),
